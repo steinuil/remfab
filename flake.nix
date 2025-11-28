437d1088ce9320a84bf1d7d0ada8e6a4e15db8cb
steinuil <steenuil.owl@gmail.com>
2025-11-28 23:41:43 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      naersk,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naerskBuildPackage = (pkgs.callPackage naersk { }).buildPackage;
+      in
+      {
+        defaultPackage = naerskBuildPackage { src = ./.; };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+          ];
+
+          buildInputs = with pkgs; [
+            rust-analyzer
+            clippy
+            lldb
+            rustfmt
+          ];
+        };
+      }
+    );
+}
